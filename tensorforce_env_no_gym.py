a580294c61c14a7de2ccbeea6d58235542ff56f3
SestoAle <alessandrosestini92@gmail.com>
2021-05-11 12:36:52 +0200

    removed gym dependence from env

---
+++ tensorforce_env_no_gym.py
@@ -0,0 +1,201 @@
+import numpy as np
+import gym
+
+from enum import IntEnum
+from gym import spaces
+from tensorflow.keras.losses import CategoricalCrossentropy
+from grid_drawer import AgentSprite, Drawer
+
+
+class DyadicConvnetGymEnv():
+
+    metadata = {'render-modes': ['human']}
+
+    class Actions(IntEnum):
+        down = 10
+        up_top_left = 11
+        up_bottom_left = 12
+        up_top_right = 13
+        up_bottom_right = 14
+
+    def __init__(self, dataset, images, labels, layers, max_steps, visualize=False, training=False, num_classes=10, tile_width=10,
+                 class_penalty=0.1, correct_class=1.0, illegal_mov=0.5, same_position=0.01, non_classified=3.0,
+                 step_reward_multiplier=0.01):
+        super(DyadicConvnetGymEnv, self).__init__()
+        self.episodes_count = 0
+        self.dataset = dataset
+        self.images = images if visualize else None
+        self.labels = labels
+        self.layers = layers
+        self.dataset_length = len(self.dataset)
+        self.num_layers = len(self.layers)
+        self.max_layer = np.max(self.layers)
+        self.min_layer = np.min(self.layers)
+        # Extracting current training image from dataset
+        self.train_image = None
+        self.image_class = None
+        # CNN representation of the extracted image
+        self.features = None
+        # Will need this for computing the reward
+        self.actions = DyadicConvnetGymEnv.Actions
+        # Single action space
+        """self.action_space = spaces.Dict({'classification': spaces.Discrete(len(self.ground_truth)),
+                                         'movement': spaces.Discrete(len(self.actions))
+                                         })"""
+        self.num_actions = num_classes + len(self.actions)
+        self.action_space = spaces.Discrete(n=self.num_actions)
+        # 64 conv features
+        self.observation_space = spaces.Dict({'features': spaces.Box(low=0.0, high=1.0, shape=(147,), dtype=np.float32)
+                                              })
+        self.step_count = 0
+        self.agent_pos = None
+        self.ep_visited = None
+        self.agent_classification = None
+        self.one_hot_action = None
+        self.max_steps = max_steps
+        self.agent_reward_loss = CategoricalCrossentropy()
+        self.class_reward = 0.0
+        self.mov_reward = 0.0
+        self.last_reward = [0.0]
+        self.last_action = None
+        self.training = training
+        # Reward setup
+        self.class_penalty = class_penalty
+        self.correct_class = correct_class
+        self.illegal_mov = illegal_mov
+        self.same_position = same_position
+        self.non_classified = non_classified
+        self.step_reward_multiplier = step_reward_multiplier
+        # Drawing
+        if training:
+            self.visualize = False
+            self.agent_sprite = None
+            self.drawer = None
+        else:
+            self.visualize = visualize
+            self.agent_sprite = AgentSprite(rect_width=tile_width, layers=self.layers, pos=(0, 0, 0)) if self.visualize else None
+            self.drawer = Drawer(self.agent_sprite, layers=self.layers, tile_width=tile_width) if self.visualize else None
+    
+    def max_episode_timesteps(self):
+        return self.max_steps
+
+    def execute(self, actions):
+        action = actions
+        self.step_count += 1
+        done = False
+        self.class_reward = 0.0
+        self.mov_reward = 0.0
+        old_pos = self.agent_pos
+        # New agent position based on the movement action
+        if action == self.actions.down:
+            if self.agent_pos[0] < self.max_layer:
+                self.agent_pos = (self.agent_pos[0] + 1,
+                                  int(self.agent_pos[1]/2),
+                                  int(self.agent_pos[2]/2))
+        elif action == self.actions.up_top_left:
+            if self.agent_pos[0] > self.min_layer:
+                self.agent_pos = (self.agent_pos[0] - 1,
+                                  2*self.agent_pos[1],
+                                  2*self.agent_pos[2])
+        elif action == self.actions.up_top_right:
+            if self.agent_pos[0] > self.min_layer:
+                self.agent_pos = (self.agent_pos[0] - 1,
+                                  2*self.agent_pos[1] + 1,
+                                  2*self.agent_pos[2])
+        elif action == self.actions.up_bottom_left:
+            if self.agent_pos[0] > self.min_layer:
+                self.agent_pos = (self.agent_pos[0] - 1,
+                                  2*self.agent_pos[1],
+                                  2*self.agent_pos[2] + 1)
+        elif action == self.actions.up_bottom_right:
+            if self.agent_pos[0] > self.min_layer:
+                self.agent_pos = (self.agent_pos[0] - 1,
+                                  2*self.agent_pos[1] + 1,
+                                  2*self.agent_pos[2] + 1)
+        # If agent classifies, end the episode
+        else:
+            self.class_reward = self.correct_class if action == self.image_class else -self.class_penalty
+            done = True
+
+        if self.agent_pos not in self.ep_visited:
+            self.ep_visited.append(self.agent_pos)
+            new_state = 0.05
+        else:
+            new_state = 0.0
+
+        if self.visualize:
+            self.agent_sprite.move(self.agent_pos)
+            self.drawer.render(agent=self.agent_sprite, img=self.train_image, label=int(self.image_class),
+                               predicted=action if int(action) < 10 else None, first_step=False)
+
+        if self.training:
+            if self.step_count >= self.max_steps:
+                done = True
+
+        # Punishing the agent for illegal actions
+        if old_pos[0] == self.layers[0] and action in [self.actions.up_bottom_right, self.actions.up_top_right,
+                                                       self.actions.up_top_left, self.actions.up_bottom_left]:
+            self.mov_reward = -self.illegal_mov
+        elif old_pos[0] == self.layers[self.num_layers - 1] and action == self.actions.down:
+            self.mov_reward = -self.illegal_mov
+        else:
+            self.mov_reward = 0.0
+        # If agent classifies well but stays in the same position it's ok, while if classification is wrong, it is encouraged to move
+        if self.agent_pos == old_pos and action != self.image_class:
+            self.mov_reward -= self.same_position
+
+        reward = self.class_reward + self.mov_reward + self.step_reward_multiplier*self.step_count + new_state
+        if self.step_count == self.max_steps and int(action) >= 10:
+            reward -= self.non_classified
+        # Adjusting parameters for new observation
+        self.one_hot_action = [1.0 if x == action else 0.0 for x in range(self.num_actions)]
+        self.last_reward = [reward]
+        self.last_action = action
+        obs = self.gen_obs()
+        # Why {}?
+        return obs, done, reward
+
+    def reset(self):
+        # New image extraction
+        self.features = self.dataset[self.episodes_count]
+        if self.visualize:
+            self.train_image = self.images[self.episodes_count]
+        self.image_class = int(self.labels[self.episodes_count])
+        # Go to next index
+        self.episodes_count = (self.episodes_count + 1) % self.dataset_length
+        # Agent starting position encoded as (layer, x, y)
+        starting_layer = np.random.choice(self.layers)
+        starting_x = np.random.randint(0, self.features[starting_layer].shape[0]) if starting_layer != 4 else 0
+        starting_y = np.random.randint(0, self.features[starting_layer].shape[0]) if starting_layer != 4 else 0
+        #starting_x = pow(2, len(self.layers) - starting_layer - 1)
+        #starting_y = pow(2, len(self.layers) - starting_layer - 1)
+        self.agent_pos = (starting_layer, starting_x, starting_y)
+        self.ep_visited = []
+        self.ep_visited.append(self.agent_pos)
+        self.last_reward = [0.0]
+        self.one_hot_action = [0.0 for x in range(self.num_actions)]
+        self.step_count = 0
+        obs = self.gen_obs()
+        if self.visualize:
+            self.agent_sprite.move(self.agent_pos)
+            self.drawer.render(agent=self.agent_sprite, img=self.train_image, label=int(self.image_class),
+                               predicted=None, first_step=True)
+
+        return obs
+
+    def gen_obs(self):
+        # Adding random gaussian noise to features vector
+        feats = self.features[self.agent_pos[0]][self.agent_pos[1]][self.agent_pos[2]]
+        obs = {
+            'features': np.concatenate((feats, self.features[4][0][0], self.agent_pos, self.one_hot_action, self.last_reward),
+                                       axis=0)
+        }
+
+        return obs
+
+    def set_agent_classification(self, value):
+        if self.agent_classification is not None:
+            self.right_old_class = np.max(self.agent_classification) if np.argmax(self.agent_classification) < 10 else 0.0
+        else:
+            self.right_old_class = 0.0
+        self.agent_classification = value
---
+++ tensorforce_test_no_gym.py
@@ -0,0 +1,264 @@
+import copy
+import sys
+import json
+
+import numpy as np
+import tensorflow as tf
+from tensorforce.agents import ProximalPolicyOptimization
+from tensorforce.environments import Environment
+from tensorflow.keras import datasets
+
+from tensorforce_net import DyadicConvNet
+from tensorforce_env_no_gym import DyadicConvnetGymEnv
+from utils import split_dataset, n_images_per_class_new, build_heatmap
+
+
+if __name__ == '__main__':
+    with tf.device('/device:GPU:0'):
+        class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+                       'dog', 'frog', 'horse', 'ship', 'truck']
+        # Network hyperparameters
+        batch_size = 50
+        sampling_ratio = 0.99
+        discount = 0.999
+        num_classes = 10
+        lstm_units = 128
+        lstm_horizon = 5
+        steps_per_episode = 75
+        policy_lr = 5e-6
+        baseline_lr = 1e-4
+        e_r = 0.1
+        split_ratio = 0.8
+        # Reward parameters
+        class_penalty = 1.0
+        correct_class = 2.0
+        illegal_mov = 0.25
+        same_position = 0.05
+        non_classified = 3.0
+        step_reward_multiplier = 0.01
+        # Control parameters
+        visualize = False
+        # Test parameters
+        layers = [0, 1, 2, 3]
+        num_epochs = 1
+        partial_dataset = False
+        if partial_dataset:
+            images_per_class = 1
+        else:
+            images_per_class = 1000
+        heatmap_needed = False
+        histogram_needed = False
+        ########################### PREPROCESSING ##############################
+        # Network initialization
+        with tf.device('/device:CPU:0'):
+            net = DyadicConvNet(num_channels=64, input_shape=(1, 32, 32, 3))
+            net.load_weights('models/model_CIFAR10/20201212-125436.h5')
+            # Dataset initialization
+            (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+            test_images = np.array(test_images, dtype=np.float32)
+            if partial_dataset:
+                img_idxs = n_images_per_class_new(n=images_per_class, labels=test_labels, num_classes=len(class_names))
+                test_images = np.array([test_images[idx] for idx in img_idxs])
+                test_labels = np.array([test_labels[idx] for idx in img_idxs])
+            test_images = test_images / 255.0
+            # Initializing everything that the env requires to work properly
+            RGB_images = copy.deepcopy(test_images)
+            tmp = []
+            # We extract EVERY single representation to avoid doing it at every episode (MEMORY INTENSIVE)
+            idx = 1
+            for img in test_images:
+                sys.stdout.write(
+                    '\rComputing image {current}/{num_img}'.format(current=idx, num_img=test_images.shape[0]))
+                image = np.reshape(img, (1, img.shape[0], img.shape[1], img.shape[2]))
+                tmp.append(net.extract_features(image, active_layers=layers, last_layer=4))
+                idx += 1
+            test_images = copy.deepcopy(tmp)
+            del train_images, train_labels
+            del tmp
+        #########################################################################
+        # Environment initialization
+        environment = DyadicConvnetGymEnv(dataset=test_images,
+                                          labels=test_labels,
+                                          images=RGB_images,
+                                          layers=layers,
+                                          # TODO: this doesn't work
+                                          max_steps=15,
+                                          visualize=visualize,
+                                          training=False,
+                                          class_penalty=class_penalty,
+                                          correct_class=correct_class,
+                                          illegal_mov=illegal_mov,
+                                          same_position=same_position,
+                                          non_classified=non_classified,
+                                          step_reward_multiplier=step_reward_multiplier
+                                          )
+        num_actions = len(environment.actions)
+        #environment = Environment.create(environment=environment,
+        #                                 states=dict(
+        #                                     features=dict(type=float, shape=(147,)),
+        #                                 ),
+        #                                 actions=dict(type=int, num_values=num_actions+num_classes),
+        #                                 max_episode_timesteps=None
+        #                                 )
+        dirs = ['models/RL/20210428-125328']
+        for directory in dirs:
+            check_dir = directory + '/checkpoints/'
+            print('\nTesting {dir}'.format(dir=directory))
+            old_epochs = 27
+            agent = ProximalPolicyOptimization.load(directory=check_dir,
+                                                    filename='agent-{oe}'.format(oe=old_epochs-1),
+                                                    format='hdf5',
+                                                    environment=environment,
+                                                    agent='ppo',
+                                                    network=[
+                                                        dict(type='lstm', size=lstm_units, horizon=lstm_horizon, activation='relu'),
+                                                    ],
+                                                    baseline=[
+                                                        dict(type='lstm', size=lstm_units, horizon=lstm_horizon, activation='relu')
+                                                    ],
+                                                    baseline_optimizer=dict(optimizer='adam', learning_rate=baseline_lr),
+                                                    learning_rate=policy_lr,
+                                                    batch_size=batch_size,
+                                                    tracking=['distribution'],
+                                                    discount=discount,
+                                                    states=dict(
+                                                        features=dict(type=float, shape=(147,)),
+                                                    ),
+                                                    actions=dict(type=int, num_values=num_actions+num_classes),
+                                                    max_episode_timesteps=15,
+                                                    )
+            # Parameters for test loop
+            episode = 0
+            correct = 0
+            base_correct = 0
+            class_attempt = 0
+            not_classified = 0
+            rewards = []
+            performance = {}
+            predicted_labels = {}
+            true_labels = {}
+            baseline_labels = {}
+            class_distrib = {}
+            agent_positions = {}
+            actions = {}
+            ep_pos = []
+            ep_actions = []
+            only_baseline = []
+            class_pos = []
+            num_images = len(test_labels)
+            mov_histogram = {}
+            mov_histogram[0] = np.zeros(steps_per_episode).tolist()
+            # Test loop
+            for i in range(1, len(test_labels) + 1):
+                terminal = False
+                ep_reward = 0
+                state = environment.reset()
+                internals = agent.initial_internals()
+                current_step = 0
+                ep_pos = []
+                ep_actions = []
+                class_distrib[i] = []
+                while current_step < steps_per_episode:
+                    action, internals = agent.act(states=dict(features=state['features']), internals=internals,
+                                                  independent=True, deterministic=True)
+                    distrib = agent.tracked_tensors()['agent/policy/action_distribution/probabilities']
+                    ############### GATHERING DATA ################
+                    ep_pos.append(str(environment.agent_pos))
+                    ep_actions.append(int(action))
+                    # Marginalized distribution over classification actions
+                    class_distrib[i].append([x / sum(distrib[:10]) for x in distrib[:10]])
+                    ###############################################
+                    environment.set_agent_classification(distrib)
+                    state, terminal, reward = environment.execute(actions=action)
+                    # TODO: terminal inhibition doesn't work
+                    if int(action) >= 10:
+                        terminal = False
+                    if terminal:
+                        mov_histogram[0][current_step] += 1
+                        if action == test_labels[i - 1]:
+                            correct += 1
+                        with tf.device('/device:CPU:0'):
+                            pred = np.reshape(RGB_images[i-1], (1, 32, 32, 3))
+                            pred = net(pred)
+                        if int(action) < 10:
+                            # Add a classification attempt
+                            predicted_labels[i] = int(action)
+                        else:
+                            #class_dis = [x / sum(distrib[:10]) for x in distrib[:10]]
+                            if np.argmax(pred) == test_labels[i - 1]:
+                                base_correct += 1
+                            #class_attempt += 1
+                            only_baseline.append((i-1, int(np.argmax(pred))))
+                        true_labels[i] = int(test_labels[i - 1])
+                        baseline_labels[i] = int(np.argmax(pred))
+                        agent_positions[i] = ep_pos
+                        actions[i] = ep_actions
+                    if int(action) < 10:
+                        class_attempt += 1
+                    ep_reward += reward
+                    current_step += 1
+                rewards.append(ep_reward)
+                avg_reward = np.sum(rewards) / len(rewards)
+                if class_attempt == 0:
+                    class_attempt = 1
+                sys.stdout.write('\rTest: Episode {ep} - Last ep. reward: {last_ep} - Average reward: {cr} - Correct: {ok}% - RCA Correct: {rcaok}% - w/Baseline: {bok}%'
+                                 .format(ep=i,
+                                         last_ep=round(ep_reward, 2),
+                                         cr=round(avg_reward, 3),
+                                         ok=round((correct / i) * 100, 2),
+                                         rcaok=round((correct / class_attempt) * 100, 2),
+                                         bok=round(((correct+base_correct) / i) * 100, 2)))
+                sys.stdout.flush()
+            print('\n')
+            performance['predicted'] = predicted_labels
+            performance['true lab'] = true_labels
+            performance['baseline'] = baseline_labels
+            performance['class distr'] = class_distrib
+            performance['positions'] = agent_positions
+            performance['actions'] = actions
+            """with open(directory + '/stats/predicted_labels.json', 'w+') as f:
+                json.dump(performance, f)
+                f.close()
+            if histogram_needed:
+                with open(directory + '/stats/movement_histogram_test.json', 'w+') as f:
+                    json.dump(mov_histogram, f)"""
+            right_when_baseline_wrong = 0
+            wrong_when_baseline_right = 0
+            agent_baseline_wrong = 0
+            agent_baseline_right = 0
+            different_class = 0
+            same_class = 0
+            for i in predicted_labels.keys():
+                if predicted_labels[i] == true_labels[i] and baseline_labels[i] == true_labels[i]:
+                    agent_baseline_right += 1
+                if predicted_labels[i] == true_labels[i] and baseline_labels[i] != true_labels[i]:
+                    right_when_baseline_wrong += 1
+                if predicted_labels[i] != true_labels[i] and baseline_labels[i] == true_labels[i]:
+                    wrong_when_baseline_right += 1
+                if predicted_labels[i] != true_labels[i] and baseline_labels[i] != true_labels[i]:
+                    agent_baseline_wrong += 1
+                    if predicted_labels[i] != baseline_labels[i]:
+                        different_class += 1
+                    else:
+                        same_class += 1
+            right = 0
+            base_stats = {}
+            with open(directory + '/stats/only_baseline.json', 'w+') as f:
+                base_stats['baseline pred'] = only_baseline
+                ground_truth = []
+                for el in only_baseline:
+                    if el[1] == test_labels[el[0]]:
+                        right += 1
+                    ground_truth.append(int(test_labels[el[0]]))
+                base_stats['ground truth'] = ground_truth
+                json.dump(base_stats, f)
+                f.close()
+            print('Number of times that agent improves baseline: %d / %d' % (right_when_baseline_wrong, len(test_labels)))
+            print('Number of times that agent fails when baseline does not: %d / %d' % (wrong_when_baseline_right, len(test_labels)))
+            print('Number of times that both agent and baseline are right: %d / %d' % (agent_baseline_right, len(test_labels)))
+            print('Number of times that both agent and baseline are wrong: %d / %d' % (agent_baseline_wrong, len(test_labels)))
+            print('    where agent and baseline predict the same class: %d / %d' % (same_class, len(test_labels)))
+            print('    where agent and baseline predict different classes: %d / %d' % (different_class, len(test_labels)))
+            print('Number of times that baseline produces correct output when agent does not classify: %d / %d' % (right, len(only_baseline)))
+            if heatmap_needed:
+                build_heatmap(agent_positions, dir=directory, show=False, all_epochs=False)
